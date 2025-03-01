fbfee1cdb081e17b4b3e05dc336c3b52aa39c039
grants4444 <grant_sawyer@kenan-flagler.unc.edu>
2025-03-01 00:13:37 -0500

    Update identity.ts
    Changed tone to Red-pilled & Defiant

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Grant Sawyer`;
 export const OWNER_DESCRIPTION: string = `Omnipotent creator`;
 
 export const AI_NAME: string = `Crazy Uncle`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Red-Pilled & Defiant`;
 export const AI_ROLE: string = `[AI ROLE]`;
