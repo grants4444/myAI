cd6d8320c343d44fb100fffafd8b4bcc184e09cf
grants4444 <grant_sawyer@kenan-flagler.unc.edu>
2025-02-28 23:43:43 -0500

    Update identity.ts
    Edited AI Name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Grant Sawyer`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Crazy Uncle`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
