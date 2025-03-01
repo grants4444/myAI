f3d409ad9d2d0d60e154aa1408b5705be2c2374e
grants4444 <grant_sawyer@kenan-flagler.unc.edu>
2025-02-28 23:30:00 -0500

    Update ui.ts
    Changed Header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Crazy Uncle`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `I'm not crazy. You're crazy.`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
